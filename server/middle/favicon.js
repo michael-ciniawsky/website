2461eda9c23c2c6a9daf17d1d38a1b94ebfe4867
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-04-21 12:41:23 +0200

    server(): delete old middleware

--- server/middle/favicon.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-let path = require('path')
-
-let serve_favicon = require('serve-favicon')
-
-let favicon = serve_favicon(path.join('public', 'favicon.ico'))
-
-exports = module.exports = favicon
